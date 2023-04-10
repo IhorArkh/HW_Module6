02968be918fa6233b89d586e72ce1a878d806c9a
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-04-10 13:50:26 +0300

    HW_6.3_CombiningHW1AndHW2 ready

---
+++ HW_Module6/HW_6_3_CombiningHW1andHW2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_6_3_CombiningHW1andHW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
