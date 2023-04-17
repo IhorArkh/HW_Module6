8f415eea648ad68c291d81fc7f2a0f4ce4944d3f
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-04-17 23:45:47 +0300

    HW_6.5_UnitTests ready

---
+++ HW_Module6/HW_6_5_UnitTests/HW_6_1_WebApi(copy)/Models/Human.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HW_6_1_WebApi.Models
+{
+    public class Human
+    {
+        public int Passport { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
